52d96ae6fe52346e78785c91a7ece362d2371e2d
thimo <thimodehaan@gmail.com>
2022-09-23 14:47:23 +0200

    updated for praktical assignment

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// timer program accurate to the speed variable in ms
 let on = false;
-let speed = 10;
+let paused = false;
+const speed = 50;
 
 let timer = 0;
 let index = 0;
@@ -12,122 +14,148 @@ let indexTo = 9;
 let colorIndex = 0;
 let loopColorIndex = 0;
 
-
-let led_loop: number;
-let loaded = false;
-let ran = false
-
-
-
+// start of the function
 function run_loop() {
-
-    if (index >= indexFrom && index <= indexTo) {
-        light.setPixelColor(index, loopColor())
-        if (index == indexTo) {
-            loopColorIndex++
-            index = -1
+    // check if paused
+    if (!paused) {
+        // rainbow loop for ms timer
+        if (index >= indexFrom && index <= indexTo) {
+            light.setPixelColor(index, loopColor())
+            if (index == indexTo) {
+                loopColorIndex++
+                index = -1
+            }
         }
+
+        // color loop for loop counter "the loop amount indicator"
+        if (timer >= timerFrom && timer <= timerTo) {
+            light.setPixelColor(pixel, switchColor())
+            if (timer == timerTo) {
+                timerFrom = timerFrom + 40
+                timerTo = timerTo + 40
+                music.playTone(Note.C, BeatFraction.Half)
+                colorIndex++
+            }
+        }
+
+        // increment indexing variables
+        timer++;
+        index++;
     }
 
-
-    if (timer >= timerFrom && timer <= timerTo) {
-        light.setPixelColor(pixel, switchColor())
-        if (timer == timerTo) {
-            timerFrom = timerFrom + 40
-            timerTo = timerTo + 40
-            colorIndex++
-        }
-    }
-
-    function switchColor() {
-        switch (colorIndex) {
-            case (0):
-                return Colors.Purple;
-                break;
-            case (1):
-                return Colors.Pink;
-                break;
-            case (2):
-                return Colors.Indigo;
-                break;
-            case (3):
-                return Colors.Orange;
-                break;
-            case (4):
-                return Colors.Violet;
-                break;
-            case (5):
-                return Colors.White;
-                break;
-            default:
-                pixel++
-                light.setPixelColor(pixel - 1, Colors.Red)
-                colorIndex = 0
-                return Colors.Red;
-                break;
-        }
-    }
-    function loopColor() {
-        switch (loopColorIndex) {
-            case (0):
-                return Colors.Blue;
-                break;
-            case (1):
-                return Colors.Green;
-                break;
-            case (2):
-                return Colors.Yellow;
-                break;
-            case (3):
-                return Colors.Red;
-                break;
-            default:
-                loopColorIndex = 0
-                return Colors.Blue;
-                break;
-        }
-    }
-
-    timer++;
-    index++;
 }
 
+// switch statement for changing the color of the loop amount indicator
+function switchColor() {
+    switch (colorIndex) {
+        case (0):
+            return Colors.Purple;
+            break;
+        case (1):
+            return Colors.Pink;
+            break;
+        case (2):
+            return Colors.Indigo;
+            break;
+        case (3):
+            return Colors.Orange;
+            break;
+        case (4):
+            return Colors.Violet;
+            break;
+        case (5):
+            return Colors.White;
+            break;
+        default:
+            pixel++
+            light.setPixelColor(pixel - 1, Colors.Red)
+            colorIndex = 0
+            return Colors.Red;
+            break;
+    }
+}
+
+// switch statement for changing the color of the rainbow loop to indicate ms
+function loopColor() {
+    switch (loopColorIndex) {
+        case (0):
+            return Colors.Blue;
+            break;
+        case (1):
+            return Colors.Green;
+            break;
+        case (2):
+            return Colors.Yellow;
+            break;
+        case (3):
+            return Colors.Red;
+            break;
+        default:
+            loopColorIndex = 0
+            return Colors.Blue;
+            break;
+    }
+}
+
+//start function 
 function startInterval() {
+
     on = true;
 
     timer = 0;
     index = 0;
     pixel = 0;
 
-    led_loop = setInterval(function () {
-        run_loop()
+    let break_loop = false
+
+    // create the interval foor the loop function
+    let led_loop = setInterval(function () {
+        if (!on) {
+            break_loop = true
+        }
+        if (!break_loop) {
+            run_loop()
+        } else {
+            clearInterval(led_loop)
+        }
+
     }, speed);
+
 }
 
+// starting input
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     if (on) {
-        clearInterval(led_loop)
-        on = false;
+        on = false
     } else {
         startInterval()
     }
 })
 
-
-loops.forever(function () {
-    let force = pins.A2.analogRead();
-    console.log(force)
-    if (!on && force > 800) {
-        loaded = true
+// pausing input
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    if (paused) {
+        paused = false
+    } else {
+        paused = true
     }
-    else if (on && loaded && force > 200) {
-        clearInterval(led_loop)
-        ran = false
-        loaded = false
+})
 
-    }
-    if (loaded && force < 50 && !ran) {
-        ran = true
-        startInterval()
+// different style timer
+input.buttonsAB.onEvent(ButtonEvent.Click, function () {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j <= i; j++) {
+            light.setPixelColor(j, loopColor())
+            pause(100)
+        }
+        for (let j = i; j >= 0; j--) {
+            light.setPixelColor(j, Colors.Black)
+            pause(100)
+        }
+        // switch led color and restart the loop
+        if (i == 9) {
+            i = 0
+            loopColorIndex++
+        }
     }
 })
